4d145dc19a455170ed86e486983f433c8e2d0c55
WalidAhmed89 <walidahmedismail21@gmail.com>
2026-09-06 06:15:44 +0300

    make the client to depends on DistanceCalculator.java

--- Design-Patterns/Structural/Adapter/DistanceCalculator/Program.java
+++ Design-Patterns/Structural/Adapter/DistanceCalculator/Program.java
@@ -2,7 +2,7 @@ package Structural.Adapter.DistanceCalculator;
 
 public class Program {
     static void main(String[] args) {
-        DistanceAdapter distance = new DistanceAdapter();
+        DistanceCalculator distance = new DistanceAdapter();
         System.out.println(distance.calculateDistanceInKilometers(10));
     }
 }
