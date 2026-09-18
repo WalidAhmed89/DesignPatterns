a99dfaaa1c463c7f8419bea5b016de94dc9ae4d3
WalidAhmed89 <walidahmedismail21@gmail.com>
2026-09-19 00:10:15 +0300

    implements Decorator pattern with Coffee Example.

---
+++ Design-Patterns/Structural/Decorator/CoffeeDecorator/SugarDecorator.java
@@ -0,0 +1,17 @@
+package Structural.Decorator.CoffeeDecorator;
+
+public class SugarDecorator extends CoffeeDecorator{
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 10;
+    }
+}
